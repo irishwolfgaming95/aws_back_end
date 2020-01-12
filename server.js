7557ece9f49f2b2e93fe3931900d2dd1e1fc1daf
irishwolfgaming95 <valkyire8@gmail.com>
2020-01-11 18:09:26 -0600

    update to include express-mailer.

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express();
 const logger = require('morgan');
 app.use(logger('dev'));
 
+const mailer = require('express-mailer');
+ 
+mailer.extend(app, {
+  from: 'no-reply@example.com',
+  host: 'smtp.gmail.com', // hostname
+  secureConnection: true, // use SSL
+  port: 465, // port for secure SMTP
+  transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+  auth: {
+    user: 'valkyire8@gmail.com',
+    pass: ''
+  }
+});
 
 
 app.listen(5000, function () {
